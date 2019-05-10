280e479e97978f31d835f1e0b8f1cf5e15566875
Anthony Travisano <atravisano@ceiamerica.com>
2019-05-10 09:51:55 -0400

    Created interfaces
    Part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 // @Injectable({
 //   providedIn: 'root'
@@ -13,8 +15,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-      response.json());
+      <Album>response.json());
   }
 }
