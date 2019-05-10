e9fa3aa0fa39d24809f331de2b5f9024e0703c0e
Anthony Travisano <atravisano@ceiamerica.com>
2019-05-10 09:40:06 -0400

    Creating a Service
    they do this with the provides array (not recommended for newer verisons)

---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) =>
+      response.json());
+  }
+}
